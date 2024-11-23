fd3a529df9b9d80e025db70b1342bdc914a82272
Mabarik667f <jc.bob1412@gmail.com>
2024-11-23 22:34:53 +0300

    refactofing: move db_actions from api to crud for user app

--- src/user/api.py
+++ src/user/api.py
@@ -1,13 +1,9 @@
 import logging
-from django.db import connection
-from django.utils import timezone
-from django.contrib.auth.hashers import make_password
-from django.shortcuts import get_object_or_404
 
+from user import crud
 from user.schemas import AddRole, Register, UserOut, UserOutWithEmail
-from user.models import CustomUser, RoleForUser, Role as RoleModel
 
-from ninja_extra import ControllerBase, api_controller, route, permissions
+from ninja_extra import ControllerBase, api_controller, route
 
 logger = logging.getLogger('cons')
 
@@ -15,61 +11,22 @@ logger = logging.getLogger('cons')
 class UserAPI(ControllerBase):
     @route.post('/register', response={201: UserOutWithEmail}, permissions=[])
     def register(self, payload: Register):
-        user_data = {
-            "username": payload.username,
-            "email": payload.email,
-            "password": make_password(payload.password),
-            "is_active": True,
-            "is_staff": False,
-            "is_superuser": False,
-            "date_joined": timezone.now(),
-            "first_name": "",
-            "last_name": "",
-            "img": "/"
-        }
-        data = list(user_data.values())
-        template = ", ".join(["%s"] * len(data))
-        with connection.cursor() as cursor:
-            cursor.execute(f"CALL create_user({template})", data)
-
-        user = CustomUser.objects.get(username=payload.username)
-        return user
+        return crud.create_user(payload)
 
     @route.get('/{user_id}', response={200: UserOut}, permissions=[])
     def get_user(self, user_id: int):
-        user = get_object_or_404(CustomUser, id=user_id)
-        return user
-
+        return crud.get_user(user_id)
 
     @route.delete('/{user_id}', response={204: None})
     def del_user(self, user_id: int):
-        user = get_object_or_404(CustomUser, id=user_id)
-        user.delete()
-
+        crud.get_user(user_id).delete()
 
     @route.post('/role/{user_id}', response={201: UserOut})
     def add_role(self, user_id: int, role: AddRole):
-        role_obj = get_object_or_404(RoleModel, name=role.name)
-        user = get_object_or_404(CustomUser, id=user_id)
-
-        data = {
-            "user_id": user_id,
-            "role_id": role_obj.pk
-        }
-        data = list(data.values())
-        template = ', '.join(["%s"] * len(data))
-        with connection.cursor() as cursor:
-            cursor.execute(f"CALL add_role_for_user({template})", data)
-
-        user = get_object_or_404(CustomUser, id=user_id)
-        return user
-
+        crud.add_role(user_id, role)
+        return crud.get_user(user_id)
 
     @route.delete('/role/{user_id}', response={204: UserOut})
     def del_role(self, user_id: int, role: AddRole):
-        role_obj = get_object_or_404(RoleModel, name=role.name)
-        obj = get_object_or_404(RoleForUser, user_id=user_id, role_id=role_obj.pk)
-        obj.delete()
-
-        user = get_object_or_404(CustomUser, id=user_id)
-        return user
+        crud.del_role(user_id, role)
+        return crud.get_user(user_id)
---
+++ src/user/crud.py
@@ -0,0 +1,47 @@
+from django.db import connection
+from django.utils import timezone
+from django.contrib.auth.hashers import make_password
+from django.shortcuts import get_object_or_404
+
+from user.schemas import AddRole, Register, UserOut, UserOutWithEmail
+from user.models import CustomUser, RoleForUser, Role as RoleModel
+
+def create_user(payload: Register) -> CustomUser:
+    user_data = {
+        "username": payload.username,
+        "email": payload.email,
+        "password": make_password(payload.password),
+        "is_active": True,
+        "is_staff": False,
+        "is_superuser": False,
+        "date_joined": timezone.now(),
+        "first_name": "",
+        "last_name": "",
+        "img": "/"
+    }
+    data = list(user_data.values())
+    template = ", ".join(["%s"] * len(data))
+    with connection.cursor() as cursor:
+        cursor.execute(f"CALL create_user({template})", data)
+
+    return CustomUser.objects.get(username=payload.username)
+
+
+def get_user(user_id: int) -> CustomUser:
+    return get_object_or_404(CustomUser, id=user_id)
+
+
+def add_role(user_id: int, role: AddRole):
+    role_obj = get_object_or_404(RoleModel, name=role.name)
+
+    data = {"user_id": user_id, "role_id": role_obj.pk}
+    data = list(data.values())
+    template = ', '.join(["%s"] * len(data))
+    with connection.cursor() as cursor:
+        cursor.execute(f"CALL add_role_for_user({template})", data)
+
+
+def del_role(user_id: int, role: AddRole):
+    role_obj = get_object_or_404(RoleModel, name=role.name)
+    obj = get_object_or_404(RoleForUser, user_id=user_id, role_id=role_obj.pk)
+    obj.delete()
