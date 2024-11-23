891d227be14b4e9224faa9fdc92bcc534ef75545
Mabarik667f <jc.bob1412@gmail.com>
2024-11-23 23:05:38 +0300

    refactofing: move db_actions from api to crud for cart app

--- src/cart/api.py
+++ src/cart/api.py
@@ -1,43 +1,20 @@
-from django.db import connection
-from django.shortcuts import get_object_or_404
 from ninja_extra import ControllerBase, api_controller, route
 from ninja_jwt.authentication import JWTAuth
 
-from cart.models import CartItem
 from cart.schemas import AddCartItem, CartItemOut
-from product.models import Product
-from config.exceptions import InsufficientProductError
+from cart import crud
 
 @api_controller("/cart", tags=["cart"], auth=JWTAuth())
 class CartAPI(ControllerBase):
 
     @route.post("/",  response={201: CartItemOut})
     def add_to_cart(self, cart: AddCartItem):
-
-        product = get_object_or_404(Product, id=cart.product_id)
-        if product.count < cart.count:
-            raise InsufficientProductError()
-
-        dict_data = {
-            "product_id": cart.product_id,
-            "user_id": self.context.request.user.pk, #type: ignore
-            "count": cart.count,
-            "delivery_date": cart.delivery_date
-        }
-
-        data = list(dict_data.values())
-        template = ", ".join(["%s"] * len(data))
-        with connection.cursor() as cursor:
-            cursor.execute(f"CALL create_cart_item({template})", data)
-        obj = CartItem.objects.get(product_id=dict_data["product_id"], user_id=dict_data["user_id"])
-        return obj
+        return crud.add_to_cart(self.context.request.user.pk, cart) #type: ignore
 
     @route.delete("/{cart_item_id}", response={204: None})
     def un_cart(self, cart_item_id: int):
-        obj = get_object_or_404(CartItem, id=cart_item_id)
-        obj.delete()
+        crud.un_cart(cart_item_id)
 
     @route.get('/', response={200: list[CartItemOut]})
     def get_cart(self):
-        user_id = self.context.request.user.pk #type: ignore
-        return CartItem.objects.filter(user_id=user_id)
+        return crud.get_cart(self.context.request.user.pk) #type: ignore
---
+++ src/cart/crud.py
@@ -0,0 +1,41 @@
+from django.db import connection
+from django.db.models import QuerySet
+from django.shortcuts import get_object_or_404
+
+from cart.models import CartItem
+from cart.schemas import AddCartItem
+from product.models import Product
+from config.exceptions import InsufficientProductError
+
+
+def add_to_cart(user_id: int, cart: AddCartItem) -> CartItem:
+
+    product = get_object_or_404(Product, id=cart.product_id)
+    if product.count < cart.count:
+        raise InsufficientProductError()
+
+    dict_data = {
+        "product_id": cart.product_id,
+        "user_id": user_id,
+        "count": cart.count,
+        "delivery_date": cart.delivery_date
+    }
+
+    data = list(dict_data.values())
+    template = ", ".join(["%s"] * len(data))
+    with connection.cursor() as cursor:
+        cursor.execute(f"CALL create_cart_item({template})", data)
+    obj = CartItem.objects.get(product_id=dict_data["product_id"], user_id=dict_data["user_id"])
+    return obj
+
+
+def get_cart_item(cart_item_id: int) -> CartItem:
+    return get_object_or_404(CartItem, id=cart_item_id)
+
+
+def un_cart(cart_item_id: int) -> None:
+    get_cart_item(cart_item_id).delete()
+
+
+def get_cart(user_id: int) -> QuerySet[CartItem]:
+    return CartItem.objects.filter(user_id=user_id)
